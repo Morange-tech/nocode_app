3c962d273f58ffd5f77b2ac54d94685c9a3492e5
Morange-tech <baesara803@gmail.com>
2026-05-14 22:15:23 +0100

    updated

--- nocode_app/next.config.ts
+++ nocode_app/next.config.ts
@@ -3,5 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+console.log("INSFORGE:", process.env.NEXT_PUBLIC_INSFORGE_URL);
 
 export default nextConfig;
